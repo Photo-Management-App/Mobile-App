030ecc3bd23ccfc2e4b7397f7b464bf2d21e4931
Alicja Banaszewska <alicja.banaszewska@op.pl>
2025-04-26 00:07:52 +0200

    Added Login and Register and moving between them

--- PhotoManagementApp/app/src/main/java/com/example/photomanagementapp/ActivityRegister.java
+++ PhotoManagementApp/app/src/main/java/com/example/photomanagementapp/ActivityRegister.java
@@ -9,22 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //Go to register activity on btnNew click
+
+        //Go to login activity on btnLogin click
         findViewById(R.id.btnHaveAccount).setOnClickListener(v -> {
-            startActivity(new Intent(this, ActivityRegister.class));
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
         });
     }
 }
